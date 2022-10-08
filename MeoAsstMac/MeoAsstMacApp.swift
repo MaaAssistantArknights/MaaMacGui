f5bb76bc97d9d28cb10a2685ad92a93c247204d4
Hao Guan <10684225+hguandl@users.noreply.github.com>
2022-10-08 20:30:50 +1000

    Initial Commit

---
+++ MeoAsstMac/MeoAsstMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeoAsstMacApp.swift
+//  MeoAsstMac
+//
+//  Created by hguandl on 8/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeoAsstMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
