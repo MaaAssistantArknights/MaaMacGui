54df7aedaa75646b003afd8f06204d96734e84d5
Hao Guan <10684225+hguandl@users.noreply.github.com>
2023-11-14 15:55:54 +1000

    chore: improve setting style

--- MeoAsstMac/Settings/SystemSettingsView.swift
+++ MeoAsstMac/Settings/SystemSettingsView.swift
@@ -5,11 +5,13 @@ struct SystemSettingsView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            Toggle(isOn: $viewModel.preventSystemSleeping, label: {
+            Toggle(isOn: $viewModel.preventSystemSleeping) {
+                VStack(alignment: .leading, spacing: 2) {
                     Text("阻止系统睡眠")
-            })
-            Text("日常任务定时执行会在系统休眠之后失效, 打开此功能可以阻止系统自动睡眠")
-                .foregroundColor(.secondary)
+                    Text("日常任务定时执行会在系统休眠之后失效, 打开此功能可以阻止系统自动睡眠")
+                        .font(.caption).foregroundColor(.secondary)
+                }
+            }
         }
         .padding()
     }
