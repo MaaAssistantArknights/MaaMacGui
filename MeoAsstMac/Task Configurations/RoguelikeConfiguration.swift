29b46cab331ed1c20b346a7efd229d99a074f21d
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-11-07 21:12:51 +0800

    fix: 肉鸽配置兼容

--- MeoAsstMac/Task Configurations/RoguelikeConfiguration.swift
+++ MeoAsstMac/Task Configurations/RoguelikeConfiguration.swift
@@ -68,5 +68,6 @@ extension RoguelikeConfiguration {
         self.refresh_trader_with_dice = (try? container.decode(Bool.self, forKey: .refresh_trader_with_dice)) ?? false
         self.start_with_elite_two = (try? container.decode(Bool.self, forKey: .start_with_elite_two)) ?? false
         self.only_start_with_elite_two = (try? container.decode(Bool.self, forKey: .only_start_with_elite_two)) ?? false
+        self.difficulty = (try? container.decode(Int.self, forKey: .difficulty)) ?? -1
     }
 }
