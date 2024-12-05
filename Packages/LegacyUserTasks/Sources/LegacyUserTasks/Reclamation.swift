6b4528ddfae5b11a4dd999477594c600a86b0637
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-12-05 19:35:42 +0800

    fix: enum name

--- Packages/LegacyUserTasks/Sources/LegacyUserTasks/Reclamation.swift
+++ Packages/LegacyUserTasks/Sources/LegacyUserTasks/Reclamation.swift
@@ -9,14 +9,14 @@ import Foundation
 
 @frozen public struct LegacyReclamationConfiguration: Codable {
     public let enable: Bool
-    public let theme: ReclamationTheme
+    public let theme: LegacyReclamationTheme
     public let mode: Int
     public let tool_to_craft: String
     public let num_craft_batches: Int
     public let increment_mode: Int
 }
 
-@frozen public enum ReclamationTheme: String, Codable {
+@frozen public enum LegacyReclamationTheme: String, Codable {
     case fire = "Fire"
     case tales = "Tales"
 }
