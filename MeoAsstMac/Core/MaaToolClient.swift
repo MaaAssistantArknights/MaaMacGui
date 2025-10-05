49b9dec8fc2e212628117266ae8ae706bc7dd83e
Rain Yang <rain-yangty@qq.com>
2025-10-05 22:55:32 +0800

    feat: Add Connection Retry Limit to MaaToolClient Initialization

--- MeoAsstMac/Core/MaaToolClient.swift
+++ MeoAsstMac/Core/MaaToolClient.swift
@@ -14,8 +14,8 @@ actor MaaToolClient {
     init?(address: String) async {
         let parts = address.split(separator: ":")
         guard parts.count >= 2,
-              let portNumber = UInt16(parts[1]),
-              let port = NWEndpoint.Port(rawValue: portNumber)
+            let portNumber = UInt16(parts[1]),
+            let port = NWEndpoint.Port(rawValue: portNumber)
         else {
             return nil
         }
@@ -40,11 +40,19 @@ actor MaaToolClient {
 
         connection.start(queue: .global())
 
+        var retryCount = 0
+        let maxRetries = 20
+
         for await state in states {
             switch state {
             case .setup, .preparing, .cancelled:
                 break
             case .waiting, .failed:
+                retryCount += 1
+                if retryCount > maxRetries {
+                    connection.cancel()  // 取消连接
+                    return nil  // 达到最大重试次数，初始化失败
+                }
                 try? await Task.sleep(nanoseconds: 500_000_000)
                 connection.restart()
             case .ready:
@@ -62,13 +70,15 @@ actor MaaToolClient {
         let data = Data([0x4d, 0x41, 0x41, 0x00, 0x00, 0x04, 0x54, 0x45, 0x52, 0x4d])
 
         return try await withCheckedThrowingContinuation { continuation in
-            connection.send(content: data, completion: .contentProcessed { error in
-                if let error {
-                    continuation.resume(throwing: error)
-                } else {
-                    continuation.resume()
-                }
-            })
+            connection.send(
+                content: data,
+                completion: .contentProcessed { error in
+                    if let error {
+                        continuation.resume(throwing: error)
+                    } else {
+                        continuation.resume()
+                    }
+                })
         }
     }
 }
