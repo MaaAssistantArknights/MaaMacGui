59ae1a79dad89d67026fb523c807968839ed48fa
Hao Guan <10684225+hguandl@users.noreply.github.com>
2026-09-06 02:57:18 +0800

    depot

--- MeoAsstMac/Model/MAADepot.swift
+++ MeoAsstMac/Model/MAADepot.swift
@@ -6,41 +6,22 @@
 //
 
 import Foundation
+import JBirdCore
 
-struct MAADepot: Codable {
+struct MAADepot {
     let done: Bool
-    let arkplanner: Arkplanner
-    let lolicon: Lolicon
-    
-    struct Arkplanner: Codable {
-        let object: ArkplannerObject
-        let data: String
-    }
-    
-    struct ArkplannerObject: Codable {
-        let items: [ArkplannerItem]
-    }
-    
-    struct ArkplannerItem: Codable {
-        let id: String
-        let have: Int
-        let name: String
-    }
-    
-    struct Lolicon: Codable {
-        let object: [String: Int]
-        let data: String
-    }
+    let items: [String: Int]
 }
 
-extension MAADepot: CustomStringConvertible {
-    var contents: [String] {
-        arkplanner.object.items
-            .sorted { $0.id < $1.id }
-            .map { "\($0.name): \($0.have)" }
-    }
-    
-    var description: String {
-        contents.joined(separator: "\n")
+extension MAADepot: JSONInitializable {
+    init(json: JSON) throws {
+        done = try json["done"]
+        do {
+            items = try json["data"]
+        } catch {
+            let string: String = try json["data"]
+            let data = try JSON(jsonString: string)
+            items = try .init(json: data)
+        }
     }
 }
--- MeoAsstMac/Model/MAAViewModel.swift
+++ MeoAsstMac/Model/MAAViewModel.swift
@@ -89,7 +89,6 @@ import SwiftUI
 
     @Published var recruitConfig = RecruitConfiguration.recognition
     @Published var recruit: MAARecruit?
-    @Published var depot: MAADepot?
     @Published var operBox: MAAOperBox?
 
     // MARK: - Connection Settings
