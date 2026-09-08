704a7307d29b489704cdba4eba9d582fe73012f9
Hao Guan <10684225+hguandl@users.noreply.github.com>
2026-09-08 19:19:42 +0800

    macsck

--- MeoAsstMac/Model/MAAViewModel.swift
+++ MeoAsstMac/Model/MAAViewModel.swift
@@ -214,6 +214,9 @@ extension MAAViewModel {
             if toolsMode == .MacSCK && !CGPreflightScreenCaptureAccess() {
                 logError("未开启屏幕录制权限，请前往“系统设置” > “隐私与安全性” > “录屏与系统录音”允许MAA访问")
             }
+            if toolsMode == .MacSCK {
+                logInfo("运行过程中，请勿将游戏设置为全屏幕，或移动窗口至其他显示器")
+            }
         }
 
         let connectionProfile: String
